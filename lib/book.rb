a2a5cd4ee7e22c3a4a9d75e97e2e1e5424f9fc8e
jqwest <johnleavell@gmail.com>
2019-11-20 21:27:02 -0600

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,47 @@
+#defining a class
+class Book
+    #passing an argument in on initialization (initialize method)
+    def initialize(title)
+        @title = title
+    end
 
+    #("title" method in the book class)
+    def title
+        @title #instance variable - are properties of any particular instance of a class
+    end
+
+    #property
+    def author=(author) #setter method
+        @author = author
+    end
+
+    def author #getter method
+        @author
+    end
+    
+    #property
+    def page_count=(num)
+        @page_count = num
+    end
+
+    def page_count
+        @page_count
+    end
+
+    #property
+    def genre=(genre)
+        @genre = genre
+    end
+
+    def genre
+        @genre
+    end
+
+    #turn page method (Behavior)
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+
+end
 
